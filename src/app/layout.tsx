c053db57a27fb1c2d084aa010624c5d90f46368c
Niffe783 <nilsrusten@gmail.com>
2025-08-10 20:35:07 +0200

    Fix: proper root layout + globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Nils sin test',
-  description: 'Første Next.js-side',
+  title: 'Nils sin testside',
+  description: 'Første Next.js-side på Vercel',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
