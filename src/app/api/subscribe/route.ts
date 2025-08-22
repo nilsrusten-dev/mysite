60f20d659071d8072c118748db31fc4295e01173
nishit321 <nishit.shah44@gmail.com>
2025-08-22 11:37:55 +0530

    update

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import sgMail from '@sendgrid/mail';
 
 export async function POST(request: NextRequest) {
-  sgMail.setApiKey('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');
+  sgMail.setApiKey(process.env.SENDGRID_KEY || '');
 
   try {
     // Check if request body exists and is valid JSON
